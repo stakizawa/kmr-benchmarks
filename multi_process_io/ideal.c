71bf7a581812c395c315a357819b3e2051faf189
Shinichiro Takizawa <s.takizawa@gmail.com>
2015-07-21 22:26:28 +0900

    remove kmr functions

--- multi_process_io/ideal.c
+++ multi_process_io/ideal.c
@@ -7,8 +7,32 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <mpi.h>
-#include <kmr.h>
 #include "multi_process_io.h"
+
+static void
+add_initial_data(long *data, keyval_t *keyval)
+{
+    for (int i = 0; i < keyval->val_count; i++) {
+	data[i] = keyval->rank;
+    }
+}
+
+static void
+increase_in_memory_value(long *din, long *dout, keyval_t *keyval,
+			 MPI_Comm comm)
+{
+    for (int i = 0; i < keyval->val_count; i++) {
+	dout[i] = din[i] + 1;
+    }
+#ifdef DEBUG
+    MPI_Barrier(comm);
+    char comm_name[MPI_MAX_OBJECT_NAME];
+    int name_len;
+    MPI_Comm_get_name(comm, comm_name, &name_len);
+    fprintf(stderr, "Rank[%d]: process key[%s]-val[%ld]\n",
+    	    keyval->rank, comm_name, din[0]);
+#endif
+}
 
 
 int
@@ -20,47 +44,45 @@ main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     check_nprocs(nprocs, rank, &task_nprocs);
-    kmr_init();
-    KMR *mr = kmr_create_context(MPI_COMM_WORLD, MPI_INFO_NULL, 0);
-
-    char even_key[KEY_LEN];
-    char odd_key[KEY_LEN];
-    snprintf(even_key, KEY_LEN, "even%06d", (rank / task_nprocs + 1));
-    snprintf(odd_key,  KEY_LEN, "odd%06d",  (rank % task_nprocs + 1));
 
     MPI_Comm even_comm;
     MPI_Comm odd_comm;
     MPI_Comm_split(MPI_COMM_WORLD, rank / task_nprocs + 1, rank, &even_comm);
     MPI_Comm_split(MPI_COMM_WORLD, rank % task_nprocs + 1, rank, &odd_comm);
 
+#ifdef DEBUG
+    char even_key[MPI_MAX_OBJECT_NAME];
+    snprintf(even_key, MPI_MAX_OBJECT_NAME,
+	     "even%06d", (rank / task_nprocs + 1));
+    MPI_Comm_set_name(even_comm, even_key);
+    char odd_key[MPI_MAX_OBJECT_NAME];
+    snprintf(odd_key,  MPI_MAX_OBJECT_NAME,
+	     "odd%06d",  (rank % task_nprocs + 1));
+    MPI_Comm_set_name(odd_comm, odd_key);
+#endif
+
     keyval_t keyval0;
-    keyval0.key = even_key;
     keyval0.val_count = VAL_COUNT;
     parse_param(argc, argv, &(keyval0.val_count));
     keyval0.rank = rank;
-    KMR_KVS *kvs0 = kmr_create_kvs(mr, KMR_KV_OPAQUE, KMR_KV_OPAQUE);
-    kmr_map_once(kvs0, &keyval0, kmr_noopt, 0, add_initial_data);
+    long *data0 = (long *)malloc(sizeof(long) * keyval0.val_count);
+    add_initial_data(data0, &keyval0);
 
     double itr_times[ITERATIONS];
     for (int i = 0; i < ITERATIONS; i++) {
-	keyval_t keyval1;
-	keyval1.key = (i % 2 == 0)? odd_key : even_key;
-	keyval1.val_count = keyval0.val_count;
-	keyval1.rank = rank;
-	KMR_KVS *kvs1 = kmr_create_kvs(mr, KMR_KV_OPAQUE, KMR_KV_OPAQUE);
-
 	struct timeval ts;
 	measure_time(&ts);
-	// in real case, something should be done against
-	// even_comm or odd_comm.
-	kmr_map(kvs0, kvs1, &keyval1, kmr_noopt, increment_in_memory_value);
+	MPI_Comm comm = (i % 2 == 0) ? even_comm : odd_comm;
+	long *data1 = (long *)malloc(sizeof(long) * keyval0.val_count);
+	increase_in_memory_value(data0, data1, &keyval0, comm);
+	free(data0);
 	struct timeval te;
 	measure_time(&te);
 	itr_times[i] = calc_time_diff(&ts, &te);
 
-	kvs0 = kvs1;
+	data0 = data1;
     }
-    kmr_free_kvs(kvs0);
+    free(data0);
 
     if (rank == 0) {
 	double time_sum = 0.0;
@@ -71,8 +93,8 @@ main(int argc, char **argv)
 	printf("Total  %f\n", time_sum);
     }
 
-    kmr_free_context(mr);
-    kmr_fin();
+    MPI_Comm_free(&even_comm);
+    MPI_Comm_free(&odd_comm);
     MPI_Finalize();
     return 0;
 }
